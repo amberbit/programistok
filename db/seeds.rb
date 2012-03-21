d4b9f6e4d10805773386cf12f9912c3904ce247e
Wojtek Lukaszuk <lukaszuk.wojtek@gmail.com>
2012-03-21 12:10:49 +0100

    deleted files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,3 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
-admin = Admin.new({ :email => 'admin@example.com', :password => 'password', :password_confirmation => 'password'})
-
-admin.save
